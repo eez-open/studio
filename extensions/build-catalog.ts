90dccdc7470bda1ebad658d764bd3247e9ba59c5
Martin Vladic <martin.vladic@gmail.com>
2018-08-14 07:45:28 +0200

    #33 catalog version

--- extensions/build-catalog.ts
+++ extensions/build-catalog.ts
@@ -20,8 +20,8 @@ const EXTENSION_REPOSITORIES = [
     }
 ];
 
-(async () => {
-    const repositoryCatalogs = await Promise.all(
+function getRepositoryCatalogs() {
+    return Promise.all(
         EXTENSION_REPOSITORIES.map(async repository => {
             const paths = await globby(repository.local + "/**/*.zip");
 
@@ -61,36 +61,82 @@ const EXTENSION_REPOSITORIES = [
             );
         })
     );
+}
 
-    const catalog = [].concat.apply([], repositoryCatalogs);
-
-    writeFile("catalog.json", JSON.stringify(catalog, undefined, 4), "utf8", err => {
-        if (err) {
-            console.error(err);
-        } else {
-            var output = createWriteStream("catalog.zip");
-
-            var archive = archiver("zip", {
-                zlib: {
-                    level: 9
-                }
-            });
-
-            archive.on("warning", function(err: any) {
-                console.warn(err);
-            });
-
-            archive.on("error", function(err: any) {
-                console.error(err);
-            });
-
-            archive.pipe(output);
-
-            archive.file("catalog.json", {
-                name: "catalog.json"
-            });
-
-            archive.finalize();
-        }
+function writeCatalog(catalog: any) {
+    return new Promise((resolve, reject) => {
+        writeFile("catalog.json", JSON.stringify(catalog, undefined, 4), "utf8", err => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
     });
+}
+
+function zipCatalog() {
+    return new Promise((resolve, reject) => {
+        var output = createWriteStream("catalog.zip");
+
+        var archive = archiver("zip", {
+            zlib: {
+                level: 9
+            }
+        });
+
+        archive.on("warning", function(err: any) {
+            console.warn(err);
+        });
+
+        archive.on("error", function(err: any) {
+            reject(err);
+        });
+
+        archive.pipe(output);
+
+        archive.file("catalog.json", {
+            name: "catalog.json"
+        });
+
+        output.on("close", function() {
+            resolve();
+        });
+
+        archive.finalize();
+    });
+}
+
+function writeCatalogVersion(catalogVersion: any) {
+    return new Promise((resolve, reject) => {
+        writeFile(
+            "catalog-version.json",
+            JSON.stringify(catalogVersion, undefined, 4),
+            "utf8",
+            err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            }
+        );
+    });
+}
+
+(async () => {
+    try {
+        const repositoryCatalogs = await getRepositoryCatalogs();
+        const catalog = [].concat.apply([], repositoryCatalogs);
+        await writeCatalog(catalog);
+
+        await zipCatalog();
+
+        const catalogVersion = {
+            lastModified: new Date()
+        };
+        await writeCatalogVersion(catalogVersion);
+    } catch (err) {
+        console.error(err);
+    }
 })();
