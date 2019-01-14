ee214a8977bff2cda9f6f5bf0e9f5b44be4e80c6
Martin Vladic <martin.vladic@gmail.com>
2019-01-14 09:40:47 +0100

    db query as service

---
+++ packages/db-services/query.ts
@@ -0,0 +1,28 @@
+import { service } from "eez-studio-shared/service";
+
+import { db } from "eez-studio-shared/db";
+
+interface Params {
+    query: string;
+    args: any[];
+}
+
+interface Result {
+    err: any;
+    rows: any[];
+}
+
+export default service<Params, Result>("db-services/query", async (inputParams: Params) => {
+    try {
+        const rows = db.prepare(inputParams.query).all(...inputParams.args);
+        return {
+            err: null,
+            rows
+        };
+    } catch (err) {
+        return {
+            err,
+            rows: []
+        };
+    }
+});
--- packages/eez-studio-shared/db.ts
+++ packages/eez-studio-shared/db.ts
@@ -16,62 +16,100 @@ if (isRenderer()) {
     };
 } else {
     ({ getDbPath, setDbPath } = require("main/settings") as typeof MainSettingsModule);
-
-    const { ipcMain } = require("electron");
-
-    ipcMain.on(
-        "dbQueryTask",
-        (event: Electron.Event, taskId: string, query: string, ...args: any[]) => {
-            try {
-                const rows = db.prepare(query).all(...args);
-                event.sender.send("dbQueryTask" + taskId, null, rows);
-            } catch (err) {
-                event.sender.send("dbQueryTask" + taskId, err);
-            }
-        }
-    );
 }
 
 export let db = new Database(getDbPath());
-
 db.defaultSafeIntegers();
 
-let dbQueryTaskId = 1;
+// // DB query is executed in Main process
+// if (!isRenderer()) {
+//     const { ipcMain } = require("electron");
+
+//     ipcMain.on(
+//         "dbQueryTask",
+//         (event: Electron.Event, taskId: string, query: string, ...args: any[]) => {
+//             try {
+//                 const rows = db.prepare(query).all(...args);
+//                 event.sender.send("dbQueryTask" + taskId, null, rows);
+//             } catch (err) {
+//                 event.sender.send("dbQueryTask" + taskId, err);
+//             }
+//         }
+//     );
+// }
+
+// let dbQueryTaskId = 1;
+
+// export function dbQuery(query: string) {
+//     return {
+//         all: async (...args: any[]): Promise<any[]> => {
+//             return new Promise<any>((resolve, reject) => {
+//                 const taskId = dbQueryTaskId++;
+
+//                 EEZStudio.electron.ipcRenderer.once(
+//                     "dbQueryTask" + taskId,
+//                     (event: Electron.Event, err: any, rows: any[]) => {
+//                         if (err) {
+//                             reject(err);
+//                         } else {
+//                             for (const row of rows) {
+//                                 for (const key in row) {
+//                                     if (row.hasOwnProperty(key)) {
+//                                         const value = row[key];
+//                                         if (value && typeof value === "object") {
+//                                             const low = row[key].low;
+//                                             const high = row[key].high;
+//                                             if (low !== undefined && high !== undefined) {
+//                                                 row[key] = Database.Integer.fromBits(low, high);
+//                                             }
+//                                         }
+//                                     }
+//                                 }
+//                             }
+
+//                             resolve(rows);
+//                         }
+//                     }
+//                 );
+
+//                 EEZStudio.electron.ipcRenderer.send("dbQueryTask", taskId, query, ...args);
+//             });
+//         }
+//     };
+// }
+
+// Execute DB query as service (inside Renderer process).
+
+import dbQueryService from "db-services/query";
 
 export function dbQuery(query: string) {
     return {
         all: async (...args: any[]): Promise<any[]> => {
-            return new Promise<any>((resolve, reject) => {
-                const taskId = dbQueryTaskId++;
+            const { err, rows } = await dbQueryService({
+                query,
+                args
+            });
 
-                EEZStudio.electron.ipcRenderer.once(
-                    "dbQueryTask" + taskId,
-                    (event: Electron.Event, err: any, rows: any[]) => {
-                        if (err) {
-                            reject(err);
-                        } else {
-                            for (const row of rows) {
-                                for (const key in row) {
-                                    if (row.hasOwnProperty(key)) {
-                                        const value = row[key];
-                                        if (value && typeof value === "object") {
-                                            const low = row[key].low;
-                                            const high = row[key].high;
-                                            if (low !== undefined && high !== undefined) {
-                                                row[key] = Database.Integer.fromBits(low, high);
-                                            }
-                                        }
-                                    }
+            if (err) {
+                throw err;
+            } else {
+                for (const row of rows) {
+                    for (const key in row) {
+                        if (row.hasOwnProperty(key)) {
+                            const value = row[key];
+                            if (value && typeof value === "object") {
+                                const low = row[key].low;
+                                const high = row[key].high;
+                                if (low !== undefined && high !== undefined) {
+                                    row[key] = Database.Integer.fromBits(low, high);
                                 }
                             }
-
-                            resolve(rows);
                         }
                     }
-                );
+                }
 
-                EEZStudio.electron.ipcRenderer.send("dbQueryTask", taskId, query, ...args);
-            });
+                return rows;
+            }
         }
     };
 }
