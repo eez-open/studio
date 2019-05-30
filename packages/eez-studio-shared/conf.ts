5feb29d77ad501f460390abc1245a37c99c6d80b
Martin Vladic <martin.vladic@gmail.com>
2019-05-30 15:48:33 +0200

    Removing the folders that are now repositories

--- packages/eez-studio-shared/conf.ts
+++
@@ -1,3 +0,0 @@
-export const SETTINGS_FILE_NAME = ".eezstudio.json";
-export const DEFAULT_DB_NAME = "storage.db";
-export const EXTENSIONS_FOLDER_NAME = "extensions";
--- packages/eez-studio-shared/roundNumber.ts
+++
@@ -1,9 +0,0 @@
-export function roundNumber(value: number, digits: number) {
-    digits = digits - (Math.floor(Math.log(Math.abs(value)) / Math.log(10)) + 1);
-    if (digits < 0) {
-        digits = 0;
-    } else if (digits > 32) {
-        digits = 32;
-    }
-    return parseFloat(value.toFixed(digits));
-}
