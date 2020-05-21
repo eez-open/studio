6b402b2624933d44b9a0f451198cb42840dbb293
Martin Vladic <martin.vladic@gmail.com>
2020-05-21 19:39:25 +0200

    Update StartPage.tsx

--- packages/instrument/bb3/components/StartPage.tsx
+++ packages/instrument/bb3/components/StartPage.tsx
@@ -12,50 +12,24 @@ import { ModulesSection } from "instrument/bb3/components/ModulesSection";
 import { ShortcutsSection } from "instrument/bb3/components/ShortcutsSection";
 import { LatestHistoryItemSection } from "instrument/bb3/components/LatestHistoryItemSection";
 
+const GRID_GAP = 20;
+
 const StartPageContainer = styled.div`
     margin: 20px;
 
     display: grid;
-    grid-gap: 20px;
+    grid-gap: ${GRID_GAP}px;
     align-items: start;
 
     @media (min-width: 1200px) {
-        grid-auto-flow: column;
-        grid-template-rows: auto auto auto;
-        grid-template-columns: repeat(2, calc(100% / 2 - (2 - 1) * 20px / 2));
-
-        & > :nth-child(2) {
-            grid-row: span 2;
-        }
+        grid-template-columns: repeat(2, calc(100% / 2 - (2 - 1) * ${GRID_GAP}px / 2));
     }
 
-    @media (min-width: 1600px) {
-        grid-template-rows: repeat(10, auto);
-        grid-template-columns: repeat(3, calc(100% / 3 - (3 - 1) * 20px / 3));
+    section {
+        margin-top: 50px;
 
-        & > :nth-child(1) {
-            grid-row: 1;
-            grid-column: 1;
-        }
-
-        & > :nth-child(2) {
-            grid-row: 2 / span 9;
-            grid-column: 1;
-        }
-
-        & > :nth-child(3) {
-            grid-row: 1;
-            grid-column: 2;
-        }
-
-        & > :nth-child(4) {
-            grid-row: 2 / span 9;
-            grid-column: 2;
-        }
-
-        & > :nth-child(5) {
-            grid-row: 1 / span 10;
-            grid-column: 3;
+        &:first-child {
+            margin-top: 0;
         }
     }
 `;
@@ -70,11 +44,15 @@ export const StartPage = observer(
     }) => {
         return (
             <StartPageContainer>
-                <ShortcutsSection appStore={appStore} />
-                <LatestHistoryItemSection bb3Instrument={bb3Instrument} />
-                <FirmwareVersionSection bb3Instrument={bb3Instrument} />
-                <ModulesSection bb3Instrument={bb3Instrument} />
-                <ScriptsSection bb3Instrument={bb3Instrument} />
+                <div>
+                    <ShortcutsSection appStore={appStore} />
+                    <LatestHistoryItemSection bb3Instrument={bb3Instrument} />
+                    <FirmwareVersionSection bb3Instrument={bb3Instrument} />
+                </div>
+                <div>
+                    <ScriptsSection bb3Instrument={bb3Instrument} />
+                    <ModulesSection bb3Instrument={bb3Instrument} />
+                </div>
             </StartPageContainer>
         );
     }
