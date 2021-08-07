47724ebaed01f1f4d9230457827afce34cbcc7a2
Martin Vladic <martin.vladic@gmail.com>
2021-08-07 21:19:12 +0200

    Loop component

--- packages/project-editor/flow/action-components/index.tsx
+++ packages/project-editor/flow/action-components/index.tsx
@@ -1620,6 +1620,116 @@ registerClass(CounterActionComponent);
 
 ////////////////////////////////////////////////////////////////////////////////
 
+const LoopBody = styled.div`
+    display: flex;
+    align-items: baseline;
+    pre.single {
+        flex-grow: 1;
+        text-align: center;
+    }
+`;
+
+export class LoopActionComponent extends ActionComponent {
+    static classInfo = makeDerivedClassInfo(ActionComponent.classInfo, {
+        flowComponentId: 1021,
+
+        properties: [
+            {
+                name: "variable",
+                type: PropertyType.String,
+                propertyGridGroup: specificGroup
+            },
+            makeToggablePropertyToInput({
+                name: "from",
+                type: PropertyType.JSON,
+                propertyGridGroup: specificGroup
+            }),
+            makeToggablePropertyToInput({
+                name: "to",
+                type: PropertyType.JSON,
+                propertyGridGroup: specificGroup
+            }),
+            makeToggablePropertyToInput({
+                name: "step",
+                type: PropertyType.JSON,
+                propertyGridGroup: specificGroup
+            })
+        ],
+        icon: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M9 4.55a8 8 0 0 1 6 14.9m0 -4.45v5h5"></path>
+                <line x1="5.63" y1="7.16" x2="5.63" y2="7.17"></line>
+                <line x1="4.06" y1="11" x2="4.06" y2="11.01"></line>
+                <line x1="4.63" y1="15.1" x2="4.63" y2="15.11"></line>
+                <line x1="7.16" y1="18.37" x2="7.16" y2="18.38"></line>
+                <line x1="11" y1="19.94" x2="11" y2="19.95"></line>
+            </svg>
+        ),
+        componentHeaderColor: "#E2D96E",
+        defaultValue: {
+            from: "0",
+            step: "1"
+        }
+    });
+
+    @observable variable: string;
+    @observable from: string;
+    @observable to: string;
+    @observable step: string;
+
+    getOutputs() {
+        return [
+            ...super.getOutputs(),
+            {
+                name: "done",
+                type: PropertyType.Null
+            }
+        ];
+    }
+
+    getBody(flowContext: IFlowContext): React.ReactNode {
+        return (
+            <LoopBody className="body">
+                <div>
+                    <pre>{this.variable}</pre>
+                </div>
+                <LeftArrow />
+                <div></div>
+                <div>
+                    <pre>
+                        [ {this.from} ... {this.to} &gt;
+                        {this.step !== "1" ? ` step ${this.step}` : ""}
+                    </pre>
+                </div>
+            </LoopBody>
+        );
+    }
+
+    async execute(runningFlow: RunningFlow) {
+        // TODO
+        return undefined;
+    }
+
+    buildFlowComponentSpecific(assets: Assets, dataBuffer: DataBuffer) {
+        buildAssignableExpression(assets, dataBuffer, this, this.variable);
+    }
+}
+
+registerClass(LoopActionComponent);
+
+////////////////////////////////////////////////////////////////////////////////
+
 export class ShowPageActionComponent extends ActionComponent {
     static classInfo = makeDerivedClassInfo(ActionComponent.classInfo, {
         flowComponentId: 1022,
