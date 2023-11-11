25031f1fe17e50f4b36569cb91d3e9e0d74279db
Martin Vladic <martin.vladic@gmail.com>
2023-11-11 17:41:14 +0100

    Update main.tsx

--- packages/home/main.tsx
+++ packages/home/main.tsx
@@ -27,7 +27,7 @@ import "home/settings";
 import { extensionsCatalog } from "./extensions-manager/catalog";
 import { initProjectEditor } from "project-editor/project-editor-bootstrap";
 
-configure({ enforceActions: "observed", useProxies: "never" });
+configure({ enforceActions: "observed", useProxies: "always" });
 
 // make sure we store all the values waiting to be stored inside blur event handler
 function blurAll() {
