371b55e770e4f9dd9503f8065eb815819a226e07
Martin Vladic <martin.vladic@gmail.com>
2024-04-07 12:03:35 +0200

    #335

--- packages/project-editor/lvgl/widgets.tsx
+++ packages/project-editor/lvgl/widgets.tsx
@@ -4523,7 +4523,18 @@ export class LVGLSpinnerWidget extends LVGLWidget {
     }
 
     override lvglBuildObj(build: LVGLBuild) {
-        build.line(`lv_obj_t *obj = lv_spinner_create(parent_obj, 1000, 60);`);
+        const SPIN_TIME = 1000;
+        const ARC_LENGTH = 60;
+        if (build.project.settings.general.lvglVersion == "9.0") {
+            build.line(`lv_obj_t *obj = lv_spinner_create(parent_obj);`);
+            build.line(
+                `lv_spinner_set_anim_params(obj, ${SPIN_TIME}, ${ARC_LENGTH});`
+            );
+        } else {
+            build.line(
+                `lv_obj_t *obj = lv_spinner_create(parent_obj, ${SPIN_TIME}, ${ARC_LENGTH});`
+            );
+        }
     }
 }
 
