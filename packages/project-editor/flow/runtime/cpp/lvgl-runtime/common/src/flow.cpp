3caedbace33348191714157249c3c2c5811930a5
Jan Nieuwstad <jan.sources@nieuwstad.net>
2024-07-31 15:36:23 +0200

    LVGL key values are now passed KEY events in more generic way

--- packages/project-editor/flow/runtime/cpp/lvgl-runtime/common/src/flow.cpp
+++ packages/project-editor/flow/runtime/cpp/lvgl-runtime/common/src/flow.cpp
@@ -432,9 +432,9 @@ void flow_event_callback(lv_event_t *e) {
 #endif
     } else if (event == LV_EVENT_KEY) {
 #if LVGL_VERSION_MAJOR >= 9
-        flowPropagateValueInt32(data->flow_state, data->component_index, data->output_or_property_index, lv_indev_get_key(lv_indev_active()));
+        flowPropagateValueInt32(data->flow_state, data->component_index, data->output_or_property_index,  lv_event_get_key());
 #else
-        flowPropagateValueInt32(data->flow_state, data->component_index, data->output_or_property_index, lv_indev_get_key(lv_indev_get_act()));
+        flowPropagateValueInt32(data->flow_state, data->component_index, data->output_or_property_index, *((int32_t *)lv_event_get_param(e))));
 #endif
 #if LVGL_VERSION_MAJOR >= 9
     } else if (event == LV_EVENT_ROTARY) {
