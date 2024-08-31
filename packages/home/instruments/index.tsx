76d938968ee937a89654a06ea814d4343f85acbe
Martin Vladic <martin.vladic@gmail.com>
2024-08-31 09:29:57 +0200

    #7

--- packages/home/instruments/index.tsx
+++ packages/home/instruments/index.tsx
@@ -364,7 +364,7 @@ const Toolbar = observer(
                         <path d="M8.71 7.71 11 5.41V15a1 1 0 0 0 2 0V5.41l2.29 2.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-4-4a1 1 0 0 0-.33-.21 1 1 0 0 0-.76 0 1 1 0 0 0-.33.21l-4 4a1 1 0 1 0 1.42 1.42M21 14a1 1 0 0 0-1 1v4a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-4a1 1 0 0 0-2 0v4a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-4a1 1 0 0 0-1-1" />
                     </svg>
                 ),
-                title: "Expert selected instrument to database",
+                title: "Export to database",
                 className: "btn-secondary",
                 onClick: () => {
                     showExportDialog(this.props.instrumentsStore);
